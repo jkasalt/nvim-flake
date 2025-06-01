e91686c4f111631379cab15e18ef3e2a98e83edb
Luca Bracone <lucabr123@gmail.com>
2025-06-01 19:59:01 +0200

    refactor

--- flake.nix
+++ flake.nix
@@ -43,34 +43,30 @@
                 impure = "~/Projects/nvim-flake/nvim";
               };
 
-              start =
-                builtins.attrValues {
-                  inherit (pkgs.vimPlugins)
-                    oil-nvim
-                    lazydev-nvim
-                    mini-nvim
-                    blink-cmp
-                    which-key-nvim
-                    friendly-snippets
-                    nvim-autopairs
-                    nvim-ts-autotag
-                    nvim-lspconfig
-                    nvim-treesitter
-                    nvim-treesitter-context
-                    nvim-treesitter-textobjects
-                    vscode-nvim
-                    none-ls-nvim
-                    lsp-format-nvim
-                    rustaceanvim
-                    haskell-tools-nvim
-                    zk-nvim
-                    snacks-nvim
-                    ;
-                }
-                ++ [
-                  pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-                ];
+              start = with pkgs.vimPlugins; [
+                oil-nvim
+                lazydev-nvim
+                mini-nvim
+                blink-cmp
+                which-key-nvim
+                friendly-snippets
+                nvim-autopairs
+                nvim-ts-autotag
+                nvim-lspconfig
+                nvim-treesitter
+                nvim-treesitter-context
+                nvim-treesitter-textobjects
+                vscode-nvim
+                none-ls-nvim
+                lsp-format-nvim
+                rustaceanvim
+                haskell-tools-nvim
+                zk-nvim
+                snacks-nvim
+                nvim-treesitter.withAllGrammars
+              ];
             };
+
             extraBinPath = with pkgs; [
               lua-language-server
               vscode-langservers-extracted
