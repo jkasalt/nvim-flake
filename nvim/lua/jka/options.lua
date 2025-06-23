633bccff69b21ed812ba3a1e0bf6989b55720f68
Luca Bracone <lucabr123@gmail.com>
2025-06-23 21:24:29 +0200

    use spaces instead of tabs

--- nvim/lua/jka/options.lua
+++ nvim/lua/jka/options.lua
@@ -4,3 +4,10 @@ WK.add({
   { "L", ":bn<cr>", desc = "next buffer" },
   { "H", ":bp<cr>", desc = "previous buffer" },
 })
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
+vim.opt.expandtab = true -- Use spaces instead of tabs.
+
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations.
