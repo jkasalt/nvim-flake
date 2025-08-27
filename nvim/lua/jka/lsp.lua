57a8e05628a46e5128a319feba11938334e23e9f
Luca Bracone <lucabr123@gmail.com>
2025-08-27 17:52:13 +0200

    rm toggle virtual lines

--- nvim/lua/jka/lsp.lua
+++ nvim/lua/jka/lsp.lua
@@ -1,17 +1,11 @@
 local b = vim.lsp.buf
 
-local function toggle_virtual_lines()
-  local curr = vim.diagnostic.config().virtual_lines.current_line
-  vim.diagnostic.config({ virtual_lines = { current_line = not curr } })
-end
-
 WK.add({
   { "gd", b.definition, desc = "go to definition" },
   { "gD", b.declaration, desc = "go to declaration" },
   { "gr", b.references, desc = "list references" },
   { "ga", b.code_action, desc = "code action" },
   { "<leader>rn", b.rename, desc = "rename symbol" },
-  { "<leader>tl", toggle_virtual_lines, desc = "toggle virtual line diagnostics" },
 })
 
 require("tiny-inline-diagnostic").setup()
